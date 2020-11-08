5d4b771a78f8834ddfc58826319c30ce508cca33
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-08 15:59:41 +0100

    Added Navigation component with styling Added tachyons npm package Fixed imports and added a little styling to the front

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div className="App">
+      <Navigation />
     </div>
   );
+  }
 }
 
 export default App;
