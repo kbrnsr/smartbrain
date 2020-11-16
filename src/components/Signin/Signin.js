aabfbad4c9d15c1abdd01b7b6afed5e47deb70d7
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-11-16 11:47:32 +0100

    Converted Signin functional component to smart component Connected signin frontend to backend

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,61 +1,99 @@
-import { React } from 'react';
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
 import { PropTypes } from 'prop-types';
 
-const Signin = ({ onRouteChange }) => {
-  return (
-    <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-      <main className="pa4 black-80">
-        <div className="measure">
-          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                Email
-                <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                />
-              </label>
+class Signin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: '',
+      signInPassword: '',
+    };
+  }
+
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  };
+
+  onSubmitSignIn = () => {
+    const { onRouteChange } = this.props;
+    const { signInEmail, signInPassword } = this.state;
+    fetch('http://localhost:2999/signin', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: signInEmail, password: signInPassword }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (data === 'Success') {
+          onRouteChange('home');
+        }
+      });
+  };
+
+  render() {
+    const { onRouteChange } = this.props;
+    return (
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                  <input
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                    type="email"
+                    name="email-address"
+                    id="email-address"
+                    onChange={this.onEmailChange}
+                  />
+                </label>
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                  <input
+                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                    type="password"
+                    name="password"
+                    id="password"
+                    onChange={this.onPasswordChange}
+                  />
+                </label>
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                onClick={() => this.onSubmitSignIn()}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Sign in"
+              />
             </div>
-            <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">
-                Password
-                <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="password"
-                  name="password"
-                  id="password"
-                />
-              </label>
+            <div className="lh-copy mt3">
+              {/* eslint-disable jsx-a11y/click-events-have-key-events */}
+              {/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */}
+              <p
+                onClick={() => onRouteChange('register')}
+                className="f6 link dim black db pointer"
+              >
+                Register
+              </p>
+              {/* eslint-enable jsx-a11y/click-events-have-key-events */}
+              {/* eslint-enable jsx-a11y/no-noninteractive-element-interactions */}
             </div>
-          </fieldset>
-          <div className="">
-            <input
-              onClick={() => onRouteChange('home')}
-              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-              type="submit"
-              value="Sign in"
-            />
           </div>
-          <div className="lh-copy mt3">
-            {/* eslint-disable jsx-a11y/click-events-have-key-events */}
-            {/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */}
-            <p
-              onClick={() => onRouteChange('register')}
-              className="f6 link dim black db pointer"
-            >
-              Register
-            </p>
-            {/* eslint-enable jsx-a11y/click-events-have-key-events */}
-            {/* eslint-enable jsx-a11y/no-noninteractive-element-interactions */}
-          </div>
-        </div>
-      </main>
-    </article>
-  );
-};
+        </main>
+      </article>
+    );
+  }
+}
 
 Signin.propTypes = { onRouteChange: PropTypes.func.isRequired };
 
