3c7aabfabfd47f66c2c29a616ca7fe517d152325
kbrnsr <1452229+kbrnsr@users.noreply.github.com>
2020-12-14 23:18:54 +0100

    Updated packages.json

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,31 +2,29 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
-  return (
-    <div>
-      <p className="f3">
-        This magic brain will detect faces in your pictures. Give it a try.
-      </p>
-      <div className="center">
-        <div className="form center pa4 br3 shadow-5">
-          <input
-            type="text"
-            className="f4 pa2 w-70 center"
-            onChange={onInputChange}
-          />
-          <button
-            type="button"
-            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
-            onClick={onButtonSubmit}
-          >
-            Detect
-          </button>
-        </div>
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => (
+  <div>
+    <p className="f3">
+      This magic brain will detect faces in your pictures. Give it a try.
+    </p>
+    <div className="center">
+      <div className="form center pa4 br3 shadow-5">
+        <input
+          type="text"
+          className="f4 pa2 w-70 center"
+          onChange={onInputChange}
+        />
+        <button
+          type="button"
+          className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+          onClick={onButtonSubmit}
+        >
+          Detect
+        </button>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 ImageLinkForm.propTypes = {
   onInputChange: PropTypes.func.isRequired,
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,14 +1,12 @@
 import { React } from 'react';
 import { PropTypes } from 'prop-types';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className="white f3">{name}, your current entry count is...</div>
-      <div className=" white f1">{entries}</div>
-    </div>
-  );
-};
+const Rank = ({ name, entries }) => (
+  <div>
+    <div className="white f3">{name}, your current entry count is...</div>
+    <div className=" white f1">{entries}</div>
+  </div>
+);
 
 Rank.propTypes = {
   name: PropTypes.string.isRequired,
